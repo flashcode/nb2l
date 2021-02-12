d5dbc1473108c63f2844a7c41ad8adbb37f39687
Sébastien Helleu <flashcode@flashtux.org>
2021-02-12 20:33:46 +0100

    Add Makefile

--- nb2l/test_nb2l.py
+++ nb2l/test_nb2l.py
@@ -20,84 +20,82 @@
 
 """nb2l tests."""
 
+import sys
+
 import mock
 import pytest
-import sys
+
+import nb2l
 
 
 def test_nb2l_invalid():
     """Test invalid numbers."""
-    from nb2l import nb2l
     with pytest.raises(TypeError):
-        nb2l()  # pylint: disable=no-value-for-parameter
+        nb2l.nb2l()  # pylint: disable=no-value-for-parameter
 
     with pytest.raises(TypeError):
-        nb2l('123')
+        nb2l.nb2l('123')
 
     with pytest.raises(TypeError):
-        nb2l(123.5)
+        nb2l.nb2l(123.5)
 
 
 def test_nb2l_simple():
     """Test simple numbers."""
-    from nb2l import nb2l
-    assert nb2l(0) == 'zéro'
-    assert nb2l(1) == 'un'
-    assert nb2l(5) == 'cinq'
-    assert nb2l(10) == 'dix'
-    assert nb2l(100) == 'cent'
-    assert nb2l(123) == 'cent vingt-trois'
-    assert nb2l(200) == 'deux cents'
+    assert nb2l.nb2l(0) == 'zéro'
+    assert nb2l.nb2l(1) == 'un'
+    assert nb2l.nb2l(5) == 'cinq'
+    assert nb2l.nb2l(10) == 'dix'
+    assert nb2l.nb2l(100) == 'cent'
+    assert nb2l.nb2l(123) == 'cent vingt-trois'
+    assert nb2l.nb2l(200) == 'deux cents'
 
 
 def test_nb2l_large():
     """Test large numbers."""
-    from nb2l import nb2l
-    assert nb2l(123_456_789) == """\
+    assert nb2l.nb2l(123_456_789) == """\
 cent vingt-trois millions \
 quatre cent cinquante-six mille \
 sept cent quatre-vingt-neuf\
 """
-    assert nb2l(123_456_789_012) == """\
+    assert nb2l.nb2l(123_456_789_012) == """\
 cent vingt-trois milliards \
 quatre cent cinquante-six millions \
 sept cent quatre-vingt-neuf mille \
 douze\
 """
-    assert nb2l(2_000) == 'deux mille'
-    assert nb2l(2_000_000) == 'deux millions'
-    assert nb2l(2_000_000_000) == 'deux milliards'
-    assert nb2l(2_000_000_000_000) == 'deux billions'
-    assert nb2l(2_000_000_000_000_000) == 'deux billiards'
-    assert nb2l(2_000_000_000_000_000_000) == 'deux trillions'
+    assert nb2l.nb2l(2_000) == 'deux mille'
+    assert nb2l.nb2l(2_000_000) == 'deux millions'
+    assert nb2l.nb2l(2_000_000_000) == 'deux milliards'
+    assert nb2l.nb2l(2_000_000_000_000) == 'deux billions'
+    assert nb2l.nb2l(2_000_000_000_000_000) == 'deux billiards'
+    assert nb2l.nb2l(2_000_000_000_000_000_000) == 'deux trillions'
 
 
 def test_nb2l_overflow():
     """Test too big numbers."""
-    from nb2l import nb2l
     with pytest.raises(OverflowError):
-        assert nb2l(2_000_000_000_000_000_000_000)
+        assert nb2l.nb2l(2_000_000_000_000_000_000_000)
 
     with pytest.raises(OverflowError):
-        assert nb2l(2_000_000_000_000_000_000_000_000)
+        assert nb2l.nb2l(2_000_000_000_000_000_000_000_000)
 
     with pytest.raises(OverflowError):
-        assert nb2l(2_000_000_000_000_000_000_000_000_000)
+        assert nb2l.nb2l(2_000_000_000_000_000_000_000_000_000)
 
     with pytest.raises(OverflowError):
-        assert nb2l(2_000_000_000_000_000_000_000_000_000_000)
+        assert nb2l.nb2l(2_000_000_000_000_000_000_000_000_000_000)
 
 
 def test_nb2l_negative():
     """Test negative numbers."""
-    from nb2l import nb2l
-    assert nb2l(-0) == 'zéro'
-    assert nb2l(-1) == 'moins un'
-    assert nb2l(-123) == 'moins cent vingt-trois'
+    assert nb2l.nb2l(-0) == 'zéro'
+    assert nb2l.nb2l(-1) == 'moins un'
+    assert nb2l.nb2l(-123) == 'moins cent vingt-trois'
 
 
 def test_main():
-    import nb2l
+    """Test main function."""
     with pytest.raises(SystemExit):
         with mock.patch.object(sys, 'argv', ['nb2l']):
             nb2l.main()
@@ -108,7 +106,7 @@ def test_main():
 
 
 def test_init():
-    import nb2l
+    """Test init function"""
     with mock.patch.object(nb2l, 'main', return_value=0):
         with mock.patch.object(nb2l, '__name__', '__main__'):
             with mock.patch.object(sys, 'exit'):
